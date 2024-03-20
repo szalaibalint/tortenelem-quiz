334668b0286be5db22e6e705ce1be2c6abbf756b
David <horvathdavidalex@gmail.com>
2024-03-20 13:36:21 +0100

    Létrehoztam a classokat a kérdések eltárolására, amiket JSON fájlokból be lehet tölteni.

---
+++ quiz/Questions/DateQuestion.cs
@@ -0,0 +1,12 @@
+﻿namespace quiz;
+
+public class DateQuestion : Question {
+    private int answer;
+
+    public int Answer => answer;
+
+    public DateQuestion(string text, int answer) : base(text)
+    {
+        this.answer = answer;
+    }
+}
---
+++ quiz/Questions/TrueFalseQuestion.cs
@@ -0,0 +1,12 @@
+﻿namespace quiz;
+
+public class TrueFalseQuestion : Question {
+    private bool correct;
+
+    public bool IsCorrect => correct;
+
+    public TrueFalseQuestion(string text, bool correct) : base(text)
+    {
+        this.correct = correct;
+    }
+}
